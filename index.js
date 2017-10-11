027598d48b3faf50b14793548faac418720761e5
Jamb000h <jamb000h@gmail.com>
2017-10-12 01:32:12 +0300

    Switched to redis hash

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ client.on('error', error => {
 })
 
 app.get('/points', (req, res) => { // Get all points
-  client.get('points', (err, data) => {
+  client.hgetall('points', (err, data) => { // returns single object
     if(data) {
+      const points = Object.values(data) // scrap return onject keys
       res.set('Content-Type', 'text/json')
       res.status(200).send(data)
     } else {
@@ -24,15 +25,20 @@ app.get('/points', (req, res) => { // Get all points
   })
 })
 
-app.get('/points:uuid', (req, res) => { // Get a single point
-  client.get(req.params.uuid, (err, data) => {
-    if(data) {
-      res.set('Content-Type', 'text/json')
-      res.status(200).send(data)
-    } else {
-      res.status(403).end(err)
-    }
-  })
+app.get('/point', (req, res) => { // Get a single point
+  console.log(req)
+  if(req.body.uuid) { // If request has data
+    client.hget('points', req.body.uuid, (err, data) => {
+      if(data) {
+        res.set('Content-Type', 'text/json')
+        res.status(200).end(data)
+      } else {
+        res.status(500).end('Could not get point')
+      }
+    })
+  } else {
+     res.status(400).end("No UUID supplied")
+  }
 })
 
 app.post('/points', (req, res) => { // Post a new point
@@ -41,9 +47,9 @@ app.post('/points', (req, res) => { // Post a new point
     const point = req.body;
     const uuid = uuidv4() // Generate UUID
     point.uuid = uuid;
-    client.set(uuid, JSON.stringify(point, null, 2), (err, data) => { // Save to redis
+    client.hset('points', uuid, JSON.stringify(point, null, 2), (err, data) => { // Save to redis
       if(data) {
-        client.get(uuid, (err, data) => { // Get saved point from redis
+        client.hget('points', uuid, (err, data) => { // Get saved point from redis
           if(data) {
             res.set('Content-Type', 'text/json')
             res.status(200).end(data)
