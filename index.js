0f9455d069992c1ec85442c6df30a034105cc625
Jamb000h <jamb000h@gmail.com>
2017-10-12 01:41:13 +0300

    Added logging for debugging purposes

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ client.on('error', error => {
 app.get('/points', (req, res) => { // Get all points
   client.hgetall('points', (err, data) => { // Returns single object
     if(data) {
+      console.log(Object.values(data))
       const points = JSON.parse(Object.values(data)) // Scrap return object keys
       res.set('Content-Type', 'text/json')
       res.status(200).send(points)
