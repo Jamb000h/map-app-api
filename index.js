8df59d5eb383444813b9213268d0651ee5b19077
Jamb000h <jamb000h@gmail.com>
2017-10-12 00:53:13 +0300

    Added endpoint for posting a new point and getting a single point

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const fs = require('fs')
 const redis = require('redis')
 const dotenv = require('dotenv').config()
+const uuid = require('uuid/v4')
 
 const app = express();
-const client = redis.createClient();
+app.use(bodyParser)
 
+const client = redis.createClient();
 client.on('error', error => {
   console.log(error)
 })
 
-const mockData = fs.readFile('mockData.json', (err, data) => {
-  if(data) {
-    client.set('points', data, 'EX', 60*60*24);
-  }
-})
-
-app.get('/points', (req, res) => {
+app.get('/points', (req, res) => { // Get all points
   client.get('points', (err, data) => {
     if(data) {
       res.set('Content-Type', 'text/json')
@@ -25,6 +22,40 @@ app.get('/points', (req, res) => {
       res.status(403).end(err)
     }
   })
+})
+
+app.get('/points:uuid', (req, res) => { // Get a single point
+  client.get(req.params.uuid, (err, data) => {
+    if(data) {
+      res.set('Content-Type', 'text/json')
+      res.status(200).send(data)
+    } else {
+      res.status(403).end(err)
+    }
+  })
+})
+
+app.post('points', (req, res) => { // Post a new point
+  console.log(req)
+  if(req.body) { // If request has data
+    const point = req.body;
+    const uuid = uuidv4() // Generate UUID
+    point.uuid = uuid;
+    client.set(uuid, point, (err, data) => { // Save to redis
+      if(data) {
+        client.get(uuid, (err, data) => { // Get saved point from redis
+          if(data) {
+            res.set('Content-Type', 'text/json')
+            res.status(200).end(data)
+          } else {
+            res.status(500).end('Could not get saved point - save was fine though')
+          }
+        })
+      } else {
+        res.status(500).end('Could not save point')
+      }
+    });
+  }
 })
 
 app.listen(process.env.PORT || 3000)
