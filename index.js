b129e1eb1786b3cc9257c8413d3780b0821a6014
Jamb000h <jamb000h@gmail.com>
2017-10-11 23:37:52 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs')
+const redis = require('redis')
+const dotenv = require('dotenv').config()
+
+const app = express();
+const client = redis.createClient();
+
+client.on('error', error => {
+  console.log(error)
+})
+
+app.get('/points', (req, res) => {
+  client.get('points', (err, data) => {
+    if(data) {
+      res.set('Content-Type', 'text/json')
+      res.status(200).send(data)
+    } else {
+      res.status(403).end()
+    }
+  })
+})
+
+app.listen(process.env.PORT || 3000)
